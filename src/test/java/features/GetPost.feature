3720c5a404250a9c708b1aebf1770405777a7095
Nadeera Abeykoon <nabeykoon@gmail.com>
2020-04-26 20:46:54 +0530

    commit non BDD style code part 1

--- src/test/java/features/GetPost.feature
+++ src/test/java/features/GetPost.feature
@@ -2,18 +2,17 @@ Feature:
   Verify different GET operations using  REST-assured
 
   Scenario: Verify one author of the post
-    Given I perform GET operation for "/post"
-    And I perform GET for the post number "1"
-    Then I should see the author name as "Karthik kk"
+    Given I perform GET operation for "/posts"
+    Then I should see the author name as "Karthik KK"
 
   Scenario: Verify collection of authors in the post
-    Given I perform GET operation for "/post"
+    Given I perform GET operation for "/posts"
     Then I should see the author names
 
   Scenario: Verify parameter of get
-    Given I perform GET operation for "/post"
+    Given I perform GET operation for "/posts"
     Then I should verify GET parameter
 
-  Scenario: Verify queryParameter of get
-    Given I perform GET operation for "/post"
+  Scenario: Verify queryParam of get
+    Given I perform GET operation for "/posts"
     Then I should verify GET queryParameter
--- src/test/java/steps/GetPostSteps.java
+++ src/test/java/steps/GetPostSteps.java
@@ -4,25 +4,29 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseOptions;
+
+import utilities.RestAssuredExtension;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.matchesPattern;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class GetPostSteps {
+
+    private static ResponseOptions<Response> response;
+
     //Anti pattern mixing rest assured bdd and cucumber bdd together
     @Given("I perform GET operation for {string}")
     public void iPerformGETOperationFor(String url) {
-    }
-
-    @And("I perform GET for the post number {string}")
-    public void iPerformGETForThePostNumber(String postNumber) {
-        BDDStyleMethod.simpleGetPost(postNumber);
+        response = RestAssuredExtension.GetOps(url);
     }
 
     @Then("I should see the author name as {string}")
-    public void iShouldSeeTheAuthorNameAs(String arg0) {
+    public void iShouldSeeTheAuthorNameAs(String authorName) {
+        assertThat(response.getBody().jsonPath().get("author"), hasItem("Karthik KK"));
     }
 
     @Then("I should see the author names")
